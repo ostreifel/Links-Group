23bf4c0da54891dd8b3f09782fbd50f8c6b2e4b4
Otto Streifel <ottost@microsoft.com>
2017-10-06 10:56:42 -0700

    childType

--- scripts/TaskListener.ts
+++ scripts/TaskListener.ts
@@ -1,10 +1,15 @@
-import { IWorkItemNotificationListener } from "TFS/WorkItemTracking/ExtensionContracts";
+import { IWorkItemFieldChangedArgs, IWorkItemNotificationListener } from "TFS/WorkItemTracking/ExtensionContracts";
+import {  } from "./HtmlParser";
+import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
 
 export class TaskListener implements IWorkItemNotificationListener {
     public onLoaded(/*workItemLoadedArgs: IWorkItemLoadedArgs*/): void {
         throw new Error("Method not implemented.");
     }
-    public onFieldChanged(/*fieldChangedArgs: IWorkItemFieldChangedArgs*/): void {
+    public onFieldChanged(fieldChangedArgs: IWorkItemFieldChangedArgs): void {
+        this._getWorkItemType().then((wit) => {
+
+        });
         throw new Error("Method not implemented.");
     }
     public onSaved(/*savedEventArgs: IWorkItemChangedArgs*/): void {
@@ -18,5 +23,10 @@ export class TaskListener implements IWorkItemNotificationListener {
     }
     public onUnloaded(/*unloadedEventArgs: IWorkItemChangedArgs*/): void {
         throw new Error("Method not implemented.");
+    }
+    private _getWorkItemType(): Q.IPromise<string> {
+        return WorkItemFormService.getService().then((service) =>
+            service.getFieldValue("System.WorkItemType") as Q.IPromise<string>,
+        );
     }
 }
