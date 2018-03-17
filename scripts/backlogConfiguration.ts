89839e6efb45f92e29d747dd957762813a49890d
Otto Streifel <ottost@microsoft.com>
2018-03-17 12:33:55 -0700

    state map is state => meta

--- scripts/backlogConfiguration.ts
+++ scripts/backlogConfiguration.ts
@@ -13,5 +13,10 @@ export function getConfiguration(project: string): IPromise<BacklogConfiguration
 export async function getState(project: string, witName: string, metaState: "Proposed" | "Completed"): Promise<string> {
     const config = await getConfiguration(project);
     const [{states}] = config.workItemTypeMappedStates.filter((s) => s.workItemTypeName === witName);
-    return states[metaState];
+    for (const state in states) {
+        if (states[state] === metaState) {
+            return state;
+        }
+    }
+    throw new Error(`Could not find state for ${project}, ${witName}, ${metaState}`);
 }
