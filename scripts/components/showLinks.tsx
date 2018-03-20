341766ef69e7e54d5566353a37d9b4a8e4ed1ae2
Otto Streifel <ottost@microsoft.com>
2018-03-19 18:26:59 -0700

    Leave space for the info banner

--- scripts/components/showLinks.tsx
+++ scripts/components/showLinks.tsx
@@ -4,7 +4,12 @@ import { ILinkProps, Links } from "./Links";
 
 function afterRender() {
     $("#links-container .link .checkbox").attr("data-is-focusable", "false").attr("tab-index", -1);
-    VSS.resize();
+    const altMin = $(".callout").outerHeight();
+    if (altMin > $(".main-content").outerHeight()) {
+        VSS.resize(undefined, altMin + 16);
+    } else {
+        VSS.resize();
+    }
 }
 
 export function renderLinks(props: ILinkProps): Promise<void> {
